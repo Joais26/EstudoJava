0c56571b87c255c0926e30dc51d53975f14a872c
Your Name <you@example.com>
2023-01-24 14:28:46 -0300

    Exercicio.04

---
+++ EstudoJava/src/DobroTriploRaizQuadrada.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DobroTriploRaizQuadrada {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        int numero, dobro, triplo;
+        double raizQuadrada;
+
+        Scanner scan = new Scanner (System.in);
+
+        System.out.print("Informe o valor: ");
+        numero = scan.nextInt();
+
+        dobro = numero * 2;
+        triplo = numero * 3;
+        raizQuadrada = Math.sqrt(numero);
+
+        System.out.print("Dobro de " + numero + " é " + dobro + " \n");
+        System.out.print("Triplo de " + numero + " é " + triplo + " \n");
+        System.out.print("Raiz Quadrada de " + numero + " é " + raizQuadrada + " \n");
+    }
+}
