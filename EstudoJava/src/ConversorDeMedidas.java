7f90df4f338790d68be8c3ec1ad3361c228bcb9f
Your Name <you@example.com>
2023-01-24 15:48:59 -0300

    Exercicio.06

---
+++ EstudoJava/src/ConversorDeMedidas.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ConversorDeMedidas {
+    public static void main(String[] args) {
+        
+        double medida, cm, mm;
+
+        Scanner scan = new Scanner (System.in);
+
+        System.out.print("Uma distância em metros: ");
+        medida = scan.nextDouble();
+
+        cm = medida * 100;
+        mm = medida * 1000;
+
+        System.out.printf("A medida de %.0fm corresponde a %.0fcm e %.0fmm", medida, cm, mm);
+
+        scan.close();
+    }
+}
