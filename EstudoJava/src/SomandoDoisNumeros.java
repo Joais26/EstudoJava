4b6fc9845c1aa2bc3e8dbc8b1d25b1b93f878943
Your Name <you@example.com>
2023-01-24 13:53:55 -0300

    Exercicio.02

---
+++ EstudoJava/src/SomandoDoisNumeros.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SomandoDoisNumeros {
+    public static void main(String[] args) {
+
+        int n1, n2;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite o primeiro numero: ");
+        n1 = scan.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        n2 = scan.nextInt();
+
+        int soma = n1 + n2;
+
+        System.out.printf("A soma de %d + %d é %d%n", n1, n2, soma);
+    }
+}
