9e40f92e06e82d37a3c43719e659594e3ef28d4f
Your Name <you@example.com>
2023-01-26 12:48:22 -0300

    RadarEletronico

---
+++ EstudoJava/src/Exercicio18.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Exercicio18{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Infome sua velocidade: ");
+        double velocidade = scan.nextDouble();
+
+        double velocidadeMax = 80;
+        double multa = 7;
+        double totalAPagar = (velocidade - velocidadeMax) * 7;
+
+        if (velocidade > velocidadeMax){
+            System.out.print("Multado por excesso de velocidade\n");
+            System.out.printf("Multa a paga R$%.2f%n", totalAPagar);
+        }else{
+            System.out.print("Digija com cuidado");
+        }
+        scan.close();
+    }
+}
